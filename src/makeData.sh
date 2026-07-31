c28bd11c18902704b3e3201fb1ef9f524d5bbd9a
Prime47x <albrightn47@gmail.com>
2026-07-31 14:43:58 -0500

    make it so that makeData.sh puts data files in data folder

--- src/makeData.sh
+++ src/makeData.sh
@@ -8,6 +8,6 @@
 $( gcc -g makeData.c -lm -o makeData )
 for i in {1..6};
 do
-	$( ./makeData ${i} 750 >phase${i}Train )
-	$( ./makeData ${i} 250 >phase${i}Test )
+	$( ./makeData ${i} 750 >../data/phase${i}Train )
+	$( ./makeData ${i} 250 >../data/phase${i}Test )
 done
